e904c37c4e8b6510535d772bcada400703e5b59d
PianoNic <79938743+Pianonic@users.noreply.github.com>
2025-10-15 16:47:49 +0200

    feat: enhance navigation with mobile menu toggle and responsive styles

--- src/app/components/navigation/navigation.ts
+++ src/app/components/navigation/navigation.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { ThemeToggle } from '../theme-toggle/theme-toggle';
 
 @Component({
@@ -11,4 +12,16 @@ import { ThemeToggle } from '../theme-toggle/theme-toggle';
 })
 export class Navigation {
   protected readonly faGithub = faGithub;
+  protected readonly faBars = faBars;
+  protected readonly faTimes = faTimes;
+
+  mobileMenuOpen = signal(false);
+
+  toggleMobileMenu() {
+    this.mobileMenuOpen.update(value => !value);
+  }
+
+  closeMobileMenu() {
+    this.mobileMenuOpen.set(false);
+  }
 }
